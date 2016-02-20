eb5385a6e9d7a06508e8859e333f78bb43cb05b6
David Sandberg <david.o.sandberg@gmail.com>
2016-02-20 23:57:56 +0100

    Made a simple test case that checks that gradients are reproducible with the same seed in two sequential training runs

--- facenet/test/seed_test.py
+++ facenet/test/seed_test.py
@@ -4,6 +4,8 @@ import sys
 import time
 sys.path.append('../src')
 import facenet
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import array_ops
 
 tf.app.flags.DEFINE_integer('batch_size', 90,
                             """Number of images to process in a batch.""")
@@ -18,22 +20,17 @@ tf.app.flags.DEFINE_float('moving_average_decay', 0.9999,
 
 FLAGS = tf.app.flags.FLAGS
 
-
 def run_train():
   
   with tf.Graph().as_default():
   
     tf.set_random_seed(666)
-    global_step = tf.Variable(0, trainable=False)
     
     # Placeholder for input images
-    images_placeholder = tf.placeholder(tf.float32, shape=(FLAGS.batch_size, FLAGS.image_size, FLAGS.image_size, 3), name='Input')
-    
-    # Placeholder for phase_train
-    phase_train_placeholder = tf.placeholder(tf.bool, name='phase_train')
+    images_placeholder = tf.placeholder(tf.float32, shape=(FLAGS.batch_size, FLAGS.image_size, FLAGS.image_size, 3), name='input')
     
     # Build the inference graph
-    embeddings = facenet.inference_nn4_max_pool_96(images_placeholder, phase_train=phase_train_placeholder)
+    embeddings = inference_conv_test(images_placeholder)
     
     # Split example embeddings into anchor, positive and negative
     anchor, positive, negative = tf.split(0, 3, embeddings)
@@ -42,7 +39,10 @@ def run_train():
     loss = facenet.triplet_loss(anchor, positive, negative)
     
     # Build a Graph that trains the model with one batch of examples and updates the model parameters
-    train_op, grads = facenet.train(loss, global_step)
+    opt = tf.train.GradientDescentOptimizer(FLAGS.learning_rate)
+    #opt = tf.train.AdagradOptimizer(FLAGS.learning_rate)
+    grads = opt.compute_gradients(loss)
+    train_op = opt.apply_gradients(grads)
     
     # Before starting, initialize the variables.  We will 'run' this first.
     init = tf.initialize_all_variables()
@@ -57,37 +57,64 @@ def run_train():
       grads_eval = []
       all_vars = []
       for step in xrange(1):
-        batch = np.random.random((90,96,96,3))
-        feed_dict = { images_placeholder: batch, phase_train_placeholder: True }
+        batch = np.random.random((FLAGS.batch_size, FLAGS.image_size, FLAGS.image_size, 3))
+        feed_dict = { images_placeholder: batch }
         grad_tensors, grad_vars = zip(*grads)
         var_names = tf.all_variables()
         all_vars  += sess.run(var_names, feed_dict=feed_dict)
         grads_eval  += sess.run(grad_tensors, feed_dict=feed_dict)
         sess.run(train_op, feed_dict=feed_dict)
     
-    time.sleep(3)
     sess.close()
   return (var_names, all_vars, grad_vars, grads_eval)
+
+def _conv(inpOp, nIn, nOut, kH, kW, dH, dW, padType):
+  kernel = tf.Variable(tf.truncated_normal([kH, kW, nIn, nOut],
+                                           dtype=tf.float32,
+                                           stddev=1e-1), name='weights')
+  conv = tf.nn.conv2d(inpOp, kernel, [1, dH, dW, 1], padding=padType)
+  
+  biases = tf.Variable(tf.constant(0.0, shape=[nOut], dtype=tf.float32),
+                       trainable=True, name='biases')
+  bias = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+  conv1 = tf.nn.relu(bias)
+  return conv1
+
+def _affine(inpOp, nIn, nOut):
+  kernel = tf.Variable(tf.truncated_normal([nIn, nOut],
+                                           dtype=tf.float32,
+                                           stddev=1e-1), name='weights')
+  biases = tf.Variable(tf.constant(0.0, shape=[nOut], dtype=tf.float32),
+                       trainable=True, name='biases')
+  affine1 = tf.nn.relu_layer(inpOp, kernel, biases)
+  return affine1
+  
+def inference_conv_test(images):
+  conv1 = _conv(images, 3, 64, 7, 7, 2, 2, 'SAME')
+  resh1 = tf.reshape(conv1, [-1, 27648])
+  return resh1
+
+def inference_affine_test(images):
+  resh1 = tf.reshape(images, [-1, 27648])
+  affn1 = _affine(resh1, 27648, 1024)
+  affn2 = _affine(affn1, 1024, 1024)
+  affn3 = _affine(affn2, 1024, 1024)
+  affn4 = _affine(affn3, 1024, 128)
+  return affn4
 
 var_names1, all_vars1, grad_names1, all_grads1 = run_train()
 var_names2, all_vars2, grad_names2, all_grads2 = run_train()
 
 all_vars_close = [None] * len(all_vars1)
 for i in range(len(all_vars1)):
-  all_vars_close[i] = np.allclose(all_vars1[i], all_vars2[i], rtol=1.e-24)
-  print('%d var: %s'%(i, var_names1[i].op.name), all_vars_close[i])
-xxx = 1
+  all_vars_close[i] = np.allclose(all_vars1[i], all_vars2[i], rtol=1.e-16)
+  print('%d var %s: %s' % (i, var_names1[i].op.name, all_vars_close[i]))
   
 all_grads_close = [None] * len(all_grads1)
 for i in range(len(all_grads1)):
-  all_grads_close[i] = np.allclose(all_grads1[i], all_grads2[i], rtol=1.e-24)
-  print('%d grad: %s'%(i, grad_names1[i].op.name), all_grads_close[i])
+  all_grads_close[i] = np.allclose(all_grads1[i], all_grads2[i], rtol=1.e-16)
+  print('%d grad %s: %s' % (i, grad_names1[i].op.name, all_grads_close[i]))
 
-for i in range(len(all_grads_close)):
-  if all_grads_close[i]==False:
-    print(grad_names1[i].op.name)
+print('All vars close: %s' % all(all_vars_close))
+print('All grads close: %s' % all(all_grads_close))
 
-print('All vars close: ', all(all_vars_close))
-print('All grads close: ', all(all_grads_close))
-
-xxx = 1
