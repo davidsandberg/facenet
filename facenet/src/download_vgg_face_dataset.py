24f9a1896cbcf114be8436ffb21288737a6e5b5f
David Sandberg <david.o.sandberg@gmail.com>
2016-05-17 23:26:25 +0200

    Uncaught exception from io.imread

--- facenet/src/download_vgg_face_dataset.py
+++ facenet/src/download_vgg_face_dataset.py
@@ -14,44 +14,45 @@ def main():
   datasetDescriptor = '/home/david/datasets/download/vggface/vgg_face_dataset/files'
   textFileNames = os.listdir(datasetDescriptor)
   for textFileName in textFileNames:
-    with open(os.path.join(datasetDescriptor, textFileName), 'rt') as f:
-      lines = f.readlines()
-    dirName = textFileName.split('.')[0]
-    classPath = os.path.join(datasetDescriptor, dirName)
-    if not os.path.exists(classPath):
-      os.makedirs(classPath)
-    for line in lines:
-      x = line.split(' ')
-      fileName = x[0]
-      url = x[1]
-      box = np.rint(np.array(map(float, x[2:6])))  # x1,y1,x2,y2
-      imagePath = os.path.join(datasetDescriptor, dirName, fileName+'.png')
-      errorPath = os.path.join(datasetDescriptor, dirName, fileName+'.err')
-      if not os.path.exists(imagePath) and not os.path.exists(errorPath):
-        try:
-          img = io.imread(url, mode='RGB')
-        except (HTTPException, HTTPError, URLError, IOError) as e:
-          errorMessage = '{}: {}'.format(url, e)
-          saveErrorMessageFile(errorPath, errorMessage)
-        else:
+    if textFileName.endswith('.txt'):
+      with open(os.path.join(datasetDescriptor, textFileName), 'rt') as f:
+        lines = f.readlines()
+      dirName = textFileName.split('.')[0]
+      classPath = os.path.join(datasetDescriptor, dirName)
+      if not os.path.exists(classPath):
+        os.makedirs(classPath)
+      for line in lines:
+        x = line.split(' ')
+        fileName = x[0]
+        url = x[1]
+        box = np.rint(np.array(map(float, x[2:6])))  # x1,y1,x2,y2
+        imagePath = os.path.join(datasetDescriptor, dirName, fileName+'.png')
+        errorPath = os.path.join(datasetDescriptor, dirName, fileName+'.err')
+        if not os.path.exists(imagePath) and not os.path.exists(errorPath):
           try:
-            if img.ndim == 2:
-              img = toRgb(img)
-            if img.ndim != 3:
-              raise ValueError('Wrong number of image dimensions')
-            hist = np.histogram(img, 255, density=True)
-            if hist[0][0]>0.9 and hist[0][254]>0.9:
-              raise ValueError('Image is mainly black or white')
-            else:
-              # Crop image according to dataset descriptor
-              imgCropped = img[box[1]:box[3],box[0]:box[2],:]
-              # Scale to 256x256
-              imgResized = misc.imresize(imgCropped, (256,256))
-              # Save image as .png
-              misc.imsave(imagePath, imgResized)
-          except ValueError as e:
+            img = io.imread(url, mode='RGB')
+          except (HTTPException, HTTPError, URLError, IOError, ValueError) as e:
             errorMessage = '{}: {}'.format(url, e)
             saveErrorMessageFile(errorPath, errorMessage)
+          else:
+            try:
+              if img.ndim == 2:
+                img = toRgb(img)
+              if img.ndim != 3:
+                raise ValueError('Wrong number of image dimensions')
+              hist = np.histogram(img, 255, density=True)
+              if hist[0][0]>0.9 and hist[0][254]>0.9:
+                raise ValueError('Image is mainly black or white')
+              else:
+                # Crop image according to dataset descriptor
+                imgCropped = img[box[1]:box[3],box[0]:box[2],:]
+                # Scale to 256x256
+                imgResized = misc.imresize(imgCropped, (256,256))
+                # Save image as .png
+                misc.imsave(imagePath, imgResized)
+            except ValueError as e:
+              errorMessage = '{}: {}'.format(url, e)
+              saveErrorMessageFile(errorPath, errorMessage)
             
 def saveErrorMessageFile(fileName, errorMessage):
   print(errorMessage)
