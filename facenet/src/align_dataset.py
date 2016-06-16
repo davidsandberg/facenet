3b5ace8e70a45b3f8f10413716c42a95d1144afc
David Sandberg <david.o.sandberg@gmail.com>
2016-06-16 22:18:37 +0200

    Added fallback to prealigned image when face alignment fails

--- facenet/src/align_dataset.py
+++ facenet/src/align_dataset.py
@@ -6,6 +6,7 @@ import facenet
 import os
 import align_dlib
 
+
 tf.app.flags.DEFINE_string('input_dir', '', """Directory with unaligned images""")
 tf.app.flags.DEFINE_string('output_dir', '', """Directory with aligned face thumbnails.""")
 tf.app.flags.DEFINE_string('dlib_face_predictor', '~/repo/openface/models/dlib/shape_predictor_68_face_landmarks.dat',
@@ -14,6 +15,8 @@ tf.app.flags.DEFINE_integer('image_size', 110, """Image size (height, width) in 
 tf.app.flags.DEFINE_integer('face_size', 96, """Size of the face thumbnail (height, width) in pixels.""")
 tf.app.flags.DEFINE_boolean('use_new_alignment', False,
                             """Indicates if the improved alignment transformation should be used.""")
+tf.app.flags.DEFINE_string('prealigned_path', '', """Replace image with a pre-aligned version when face detection fails.""")
+tf.app.flags.DEFINE_float('prealigned_scale', 1.0, """The amount of scaling to apply to prealigned images before taking the center crop.""")
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -31,7 +34,6 @@ def main():
             filename = os.path.splitext(os.path.split(image_path)[1])[0]
             output_filename = os.path.join(output_class_dir, filename+'.png')
             if not os.path.exists(output_filename):
-                print(image_path)
                 try:
                     img = misc.imread(image_path)
                 except (IOError, ValueError, IndexError) as e:
@@ -47,8 +49,33 @@ def main():
                         aligned = align.align(FLAGS.image_size, img, landmarkIndices=landmarkIndices, 
                                               skipMulti=True, scale=scale)
                     if aligned is not None:
+                        print(image_path)
                         misc.imsave(output_filename, aligned)
-
+                    elif FLAGS.prealigned_path:
+                        # Face detection failed. Use center crop from pre-aligned dataset
+                        class_name = os.path.split(output_class_dir)[1]
+                        image_path_without_ext = os.path.join(os.path.expanduser(FLAGS.prealigned_path), 
+                                                              class_name, filename)
+                        # Find the extension of the image
+                        exts = ('jpg', 'png', 'gif')
+                        for ext in exts:
+                            temp_path = image_path_without_ext + '.' + ext
+                            image_path = ''
+                            if os.path.exists(temp_path):
+                                image_path = temp_path
+                                break
+                        try:
+                            img = misc.imread(image_path)
+                        except (IOError, ValueError, IndexError) as e:
+                            errorMessage = '{}: {}'.format(image_path, e)
+                            print(errorMessage)
+                        else:
+                            scaled = misc.imresize(img, FLAGS.prealigned_scale, interp='bilinear')
+                            sz1 = scaled.shape[1]/2
+                            sz2 = FLAGS.image_size/2
+                            cropped = scaled[(sz1-sz2):(sz1+sz2),(sz1-sz2):(sz1+sz2),:]
+                            print(image_path)
+                            misc.imsave(output_filename, cropped)
             
 if __name__ == '__main__':
     main()
