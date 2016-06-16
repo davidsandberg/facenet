e93b50a757341e5edbb44f32e75020ef84cfcd64
David Sandberg <david.o.sandberg@gmail.com>
2016-06-16 23:15:57 +0200

    Store revision info when aligning images

--- facenet/src/align_dataset.py
+++ facenet/src/align_dataset.py
@@ -1,6 +1,7 @@
 """Performs face alignment and stores face thumbnails in the output directory."""
 
 from scipy import misc
+from subprocess import Popen, PIPE
 import tensorflow as tf
 import facenet
 import os
@@ -20,14 +21,20 @@ tf.app.flags.DEFINE_float('prealigned_scale', 1.0, """The amount of scaling to a
 
 FLAGS = tf.app.flags.FLAGS
 
-def main():
+def main(argv=None):
     align = align_dlib.AlignDlib(os.path.expanduser(FLAGS.dlib_face_predictor))
     landmarkIndices = align_dlib.AlignDlib.OUTER_EYES_AND_NOSE
+    output_dir = os.path.expanduser(FLAGS.output_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    # Store some git revision info in a text file in the log directory
+    src_path,_ = os.path.split(os.path.realpath(__file__))
+    store_revision_info(src_path, output_dir, ' '.join(argv))
     dataset = facenet.get_dataset(FLAGS.input_dir)
     # Scale the image such that the face fills the frame when cropped to crop_size
     scale = float(FLAGS.face_size) / FLAGS.image_size
     for cls in dataset:
-        output_class_dir = os.path.join(os.path.expanduser(FLAGS.output_dir), cls.name)
+        output_class_dir = os.path.join(output_dir, cls.name)
         if not os.path.exists(output_class_dir):
             os.makedirs(output_class_dir)
         for image_path in cls.image_paths:
@@ -77,5 +84,24 @@ def main():
                             print(image_path)
                             misc.imsave(output_filename, cropped)
             
+def store_revision_info(src_path, output_dir, arg_string):
+  
+  # Get git hash 
+  gitproc = Popen(['git', 'rev-parse', 'HEAD'], stdout = PIPE, cwd=src_path)
+  (stdout, _) = gitproc.communicate()
+  git_hash = stdout.strip()
+
+  # Get local changes
+  gitproc = Popen(['git', 'diff', 'HEAD'], stdout = PIPE, cwd=src_path)
+  (stdout, _) = gitproc.communicate()
+  git_diff = stdout.strip()
+  
+  # Store a text file in the log directory
+  rev_info_filename = os.path.join(output_dir, 'revision_info.txt')
+  with open(rev_info_filename, "w") as text_file:
+    text_file.write('arguments: %s\n--------------------\n' % arg_string)
+    text_file.write('git hash: %s\n--------------------\n' % git_hash)
+    text_file.write('%s' % git_diff)
+
 if __name__ == '__main__':
-    main()
+    tf.app.run()
