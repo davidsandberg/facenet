5709c2f7a09cdf8ba3c855dc92bb277fa39c30d9
David Sandberg <david.o.sandberg@gmail.com>
2016-06-17 23:27:03 +0200

    Changed skipMulti to False. Shuffled dataset to support multithreading.

--- facenet/src/align_dataset.py
+++ facenet/src/align_dataset.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import facenet
 import os
 import align_dlib
+import random
 
 
 tf.app.flags.DEFINE_string('input_dir', '', """Directory with unaligned images""")
@@ -17,7 +18,7 @@ tf.app.flags.DEFINE_integer('face_size', 96, """Size of the face thumbnail (heig
 tf.app.flags.DEFINE_boolean('use_new_alignment', False,
                             """Indicates if the improved alignment transformation should be used.""")
 tf.app.flags.DEFINE_string('prealigned_path', '', """Replace image with a pre-aligned version when face detection fails.""")
-tf.app.flags.DEFINE_float('prealigned_scale', 1.0, """The amount of scaling to apply to prealigned images before taking the center crop.""")
+tf.app.flags.DEFINE_float('prealigned_scale', 0.87, """The amount of scaling to apply to prealigned images before taking the center crop.""")
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -31,13 +32,19 @@ def main(argv=None):
     src_path,_ = os.path.split(os.path.realpath(__file__))
     store_revision_info(src_path, output_dir, ' '.join(argv))
     dataset = facenet.get_dataset(FLAGS.input_dir)
+    random.shuffle(dataset)
     # Scale the image such that the face fills the frame when cropped to crop_size
     scale = float(FLAGS.face_size) / FLAGS.image_size
+    nrof_images_total = 0
+    nrof_prealigned_images = 0
+    nrof_successfully_aligned = 0
     for cls in dataset:
         output_class_dir = os.path.join(output_dir, cls.name)
         if not os.path.exists(output_class_dir):
             os.makedirs(output_class_dir)
+        random.shuffle(cls.image_paths)
         for image_path in cls.image_paths:
+            nrof_images_total += 1
             filename = os.path.splitext(os.path.split(image_path)[1])[0]
             output_filename = os.path.join(output_class_dir, filename+'.png')
             if not os.path.exists(output_filename):
@@ -51,12 +58,13 @@ def main(argv=None):
                         img = facenet.to_rgb(img)
                     if FLAGS.use_new_alignment:
                         aligned = align.align_new(FLAGS.image_size, img, landmarkIndices=landmarkIndices, 
-                                              skipMulti=True, scale=scale)
+                                              skipMulti=False, scale=scale)
                     else:
                         aligned = align.align(FLAGS.image_size, img, landmarkIndices=landmarkIndices, 
-                                              skipMulti=True, scale=scale)
+                                              skipMulti=False, scale=scale)
                     if aligned is not None:
                         print(image_path)
+                        nrof_successfully_aligned += 1
                         misc.imsave(output_filename, aligned)
                     elif FLAGS.prealigned_path:
                         # Face detection failed. Use center crop from pre-aligned dataset
@@ -64,7 +72,7 @@ def main(argv=None):
                         image_path_without_ext = os.path.join(os.path.expanduser(FLAGS.prealigned_path), 
                                                               class_name, filename)
                         # Find the extension of the image
-                        exts = ('jpg', 'png', 'gif')
+                        exts = ('jpg', 'png')
                         for ext in exts:
                             temp_path = image_path_without_ext + '.' + ext
                             image_path = ''
@@ -82,7 +90,12 @@ def main(argv=None):
                             sz2 = FLAGS.image_size/2
                             cropped = scaled[(sz1-sz2):(sz1+sz2),(sz1-sz2):(sz1+sz2),:]
                             print(image_path)
+                            nrof_prealigned_images += 1
                             misc.imsave(output_filename, cropped)
+                            
+    print('Total number of images: %d' % nrof_images_total)
+    print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
+    print('Number of pre-aligned images: %d' % nrof_prealigned_images)
             
 def store_revision_info(src_path, output_dir, arg_string):
   
