64e2d0b680ab5b601da3f5568c3721861956a584
David Sandberg <david.o.sandberg@gmail.com>
2016-07-28 16:06:54 +0200

    Added test for nn4_small2_v1. Added weight decay to nn4_small2_v1. Removed nn4_fc1.

--- facenet/test/train_test.py
+++ facenet/test/train_test.py
@@ -20,16 +20,28 @@ class TrainTest(unittest.TestCase):
     def tearDownClass(self):
         # Recursively remove the temporary directory
         shutil.rmtree(self.tmp_dir)
-        
 
-  
-  # Add tests for all models in the models dir
-  #  - Set max_nrof_epochs to zero to skip training?
-  
     def test_training_nn4(self):
         argv = ['--logs_base_dir', self.tmp_dir,
                 '--models_base_dir', self.tmp_dir,
                 '--data_dir', self.dataset_dir,
+                '--model_def', 'models.nn4',
+                '--epoch_size', '1',
+                '--max_nrof_epochs', '1',
+                '--batch_size', '6',
+                '--people_per_batch', '2',
+                '--images_per_person', '3',
+                '--lfw_pairs', self.lfw_pairs_file,
+                '--lfw_dir', self.dataset_dir,
+                '--lfw_nrof_folds', '2' ]
+        args = facenet_train.parse_arguments(argv)
+        facenet_train.main(args)
+
+    def test_training_nn4_small2_v1(self):
+        argv = ['--logs_base_dir', self.tmp_dir,
+                '--models_base_dir', self.tmp_dir,
+                '--data_dir', self.dataset_dir,
+                '--model_def', 'models.nn4_small2_v1',
                 '--epoch_size', '1',
                 '--max_nrof_epochs', '1',
                 '--batch_size', '6',
