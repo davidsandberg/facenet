9b87c771ea3db1a13a2bccb734bb4dd6d54f6e13
David Sandberg <david.o.sandberg@gmail.com>
2016-09-25 19:50:30 +0200

    Small fix for the visualization module

--- facenet/src/visualize.py
+++ facenet/src/visualize.py
@@ -20,31 +20,23 @@ def main(args):
 
     # Start with a gray image with a little noise
     np.random.seed(seed=args.seed)
-    img_noise = np.random.uniform(size=(96,96,3)) + 100.0
+    img_noise = np.random.uniform(size=(args.image_size,args.image_size,3)) + 100.0
   
     sess = tf.Session()
   
-    t_input = tf.placeholder(np.float32, name='input') # define the input tensor
+    t_input = tf.placeholder(np.float32, shape=(args.image_size,args.image_size,3), name='input') # define the input tensor
     image_mean = 117.0
     t_preprocessed = tf.expand_dims(t_input-image_mean, 0)
      
-    # Placeholder for phase_train
-    phase_train_placeholder = tf.constant(False, name='phase_train')
-  
     # Build the inference graph
-    network.inference(t_preprocessed, 128, 1.0, 
-            phase_train=phase_train_placeholder, weight_decay=0.0)
+    network.inference(t_preprocessed, 1.0, 
+            phase_train=True, weight_decay=0.0)
       
     # Create a saver for restoring variables
-    saver = tf.train.Saver(tf.trainable_variables())
+    saver = tf.train.Saver(tf.all_variables())
   
     # Restore the parameters
-    ckpt = tf.train.get_checkpoint_state(os.path.expanduser(args.model_dir))
-    #pylint: disable=no-member
-    if ckpt and ckpt.model_checkpoint_path:
-        saver.restore(sess, ckpt.model_checkpoint_path)
-    else:
-        raise ValueError('Checkpoint not found')
+    saver.restore(sess, args.model_file)
   
     layers = [op.name for op in tf.get_default_graph().get_operations() if op.type=='Conv2D']
     feature_nums = {layer: int(T(layer).get_shape()[-1]) for layer in layers}
@@ -56,7 +48,7 @@ def main(args):
   
     # Picking some internal layer. Note that we use outputs before applying the ReLU nonlinearity
     # to have non-zero gradients for features with negative initial activations.
-    layer = 'incept5b/in4_conv1x1_55/Conv2D'
+    layer = 'InceptionResnetV1/Repeat_2/block8_3/Conv2d_1x1/Conv2D'
     #layer = 'incept4b/in4_conv1x1_31/Conv2D'
     result_dir = '../data/'
     print('Number of features in layer "%s": %d' % (layer, feature_nums[layer]))
@@ -98,11 +90,13 @@ def render_naive(sess, t_input, t_obj, img0, iter_n=20, step=1.0):
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('model_dir', type=str, 
+    parser.add_argument('model_file', type=str, 
         help='Directory containing the graph definition and checkpoint files.')
     parser.add_argument('--model_def', type=str, 
         help='Model definition. Points to a module containing the definition of the inference graph.',
         default='models.nn4')
+    parser.add_argument('--image_size', type=int,
+        help='Image size (height, width) in pixels.', default=96)
     parser.add_argument('--seed', type=int,
         help='Random seed.', default=666)
     return parser.parse_args(argv)
