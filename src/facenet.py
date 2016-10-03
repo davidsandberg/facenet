86bcf92bc8a2d35d4f6556f4a6b7ac40a7d5799d
David Sandberg <david.o.sandberg@gmail.com>
2016-10-03 22:28:44 +0200

    Added DeCov loss

--- src/facenet.py
+++ src/facenet.py
@@ -37,6 +37,19 @@ def triplet_loss(anchor, positive, negative, alpha):
       
     return loss
   
+def decov_loss(xs):
+    """Decov loss as described in https://arxiv.org/pdf/1511.06068.pdf
+    'Reducing Overfitting In Deep Networks by Decorrelating Representation'
+    """
+    x = tf.reshape(xs, [int(xs.get_shape()[0]), -1])
+    m = tf.reduce_mean(x, 0, True)
+    z = tf.expand_dims(x-m, 2)
+    corr = tf.reduce_mean(tf.batch_matmul(z, tf.transpose(z, perm=[0,2,1])), 0)
+    corr_frob_sqr = tf.reduce_sum(tf.square(corr))
+    corr_diag_sqr = tf.reduce_sum(tf.square(tf.diag_part(corr)))
+    loss = 0.5*(corr_frob_sqr - corr_diag_sqr)
+    return loss 
+  
 def get_image_paths_and_labels(dataset):
     image_paths_flat = []
     labels_flat = []
