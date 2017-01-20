4bd9770ae8496b004c628c1158b9a44ca8a98d89
sutony <sutony@cht.com.tw>
2017-01-20 09:58:08 +0800

    make align utility python3-compatible

--- src/align/detect_face.py
+++ src/align/detect_face.py
@@ -26,6 +26,7 @@ https://github.com/kpzhang93/MTCNN_face_detection_alignment
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from six import string_types, iteritems
 
 import numpy as np
 import tensorflow as tf
@@ -81,10 +82,11 @@ class Network(object):
         session: The current TensorFlow session
         ignore_missing: If true, serialized weights for missing layers are ignored.
         '''
-        data_dict = np.load(data_path).item() #pylint: disable=no-member
+        data_dict = np.load(data_path, encoding='latin1').item() #pylint: disable=no-member
+
         for op_name in data_dict:
             with tf.variable_scope(op_name, reuse=True):
-                for param_name, data in data_dict[op_name].iteritems():
+                for param_name, data in iteritems(data_dict[op_name]):
                     try:
                         var = tf.get_variable(param_name)
                         session.run(var.assign(data))
@@ -99,7 +101,7 @@ class Network(object):
         assert len(args) != 0
         self.terminals = []
         for fed_layer in args:
-            if isinstance(fed_layer, basestring):
+            if isinstance(fed_layer, string_types):
                 try:
                     fed_layer = self.layers[fed_layer]
                 except KeyError:
