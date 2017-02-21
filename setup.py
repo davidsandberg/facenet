524be725fc4c6057152cad24911a05d854d80db3
David Sandberg <david.o.sandberg@gmail.com>
2017-02-21 16:55:18 +0100

    Added test package

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='facenet',
-      version='0.2',
+      version='0.1',
       description='Face Recognition using Tensorflow',
       author='David Sandberg',
       author_email='david.o.sandberg@gmail.com',
       url='https://github.com/davidsandberg/facenet/',
-      packages=['src','src.align','src.models'],
+      packages=['src','src.align','src.models', 'test'],
      )
