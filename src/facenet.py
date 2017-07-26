Some fixes for angular softmax

src/facenet.py
@@ -39,7 +39,6 @@ from tensorflow.python.training import training
 import random
 import re
 from tensorflow.python.platform import gfile
-import math
 
 def triplet_loss(anchor, positive, negative, alpha):
     """Calculate the triplet loss according to the FaceNet paper
@@ -131,7 +130,7 @@ def angular_softmax_loss_decomp(W, x, yi, m, lmbd):
     batch_size = tf.shape(x)[0]
     dot = tf.tensordot(x, W, axes=1)
     
-    x_norm = tf.norm(x, axis=1)
+    x_norm = tf.norm(x, axis=1) * tf.norm(W)
     x_norm_full = tf.expand_dims(x_norm, 1) * tf.ones((1, nrof_classes)) 
     cos_theta = (1/x_norm_full) * dot
     yi = tf.cast(yi, tf.int32)
@@ -141,8 +140,8 @@ def angular_softmax_loss_decomp(W, x, yi, m, lmbd):
     cos_theta_yi = tf.gather_nd(cos_theta, idx)
     x_psi_theta = tf.multiply(x_norm, psi(cos_theta_yi, m))
     x_cos_theta = tf.multiply(x_norm, cos_theta_yi)
-    #x_psi_cos_theta_mix = (lmbd*x_cos_theta+(1-lmbd)*x_psi_theta)
-    x_psi_cos_theta_mix = (lmbd*x_cos_theta+x_psi_theta) / (1+lmbd)
+    x_psi_cos_theta_mix = (lmbd*x_cos_theta+(1-lmbd)*x_psi_theta)
+    #x_psi_cos_theta_mix = (lmbd*x_cos_theta+x_psi_theta) / (1.0+lmbd)
     x_psi_theta_full = tf.scatter_nd(idx, x_psi_cos_theta_mix, (batch_size, nrof_classes))
     y = tf.where(index_matrix>0, x_psi_theta_full, dot)
     return y

src/train_angular_softmax.py
@@ -167,13 +167,21 @@ def main(args):
 #                 weights_initializer=tf.truncated_normal_initializer(stddev=0.1), 
 #                 weights_regularizer=slim.l2_regularizer(args.weight_decay),
 #                 scope='Logits', reuse=False)
+
+        #final_lambda = 0.94
+        lambda_decay_start_step = 0
+    
+        nrof_steps = tf.cast(int(args.max_nrof_epochs * len(image_list)) // args.batch_size, tf.float32)
+        margin_lambda = tf.minimum(1.0, 1.0 - (tf.cast(global_step, tf.float32)-lambda_decay_start_step)*
+            (1.0-args.final_lambda)/(nrof_steps-lambda_decay_start_step))
+
         nrof_classes = len(train_set)
         weights = tf.get_variable('softmax_weights', shape=(args.embedding_size,nrof_classes), dtype=tf.float32,
             initializer=tf.truncated_normal_initializer(stddev=0.1),
             regularizer=slim.l2_regularizer(args.weight_decay), trainable=True)
-        logits = facenet.angular_softmax_loss_decomp(weights, prelogits, label_batch, 1)
+        logits = facenet.angular_softmax_loss_decomp(weights, prelogits, label_batch, args.angular_softmax_type, margin_lambda)
         
-        normalize_op = tf.assign(weights, tf.nn.l2_normalize(weights, 1))
+        #normalize_op = tf.assign(weights, tf.nn.l2_normalize(weights, 1))
 
         embeddings = tf.nn.l2_normalize(prelogits, 1, 1e-10, name='embeddings')
 
@@ -257,7 +265,7 @@ def main(args):
                 step2 = train(args, sess, epoch, image_list, label_list, index_dequeue_op, enqueue_op, image_paths_placeholder, labels_placeholder,
                     learning_rate_placeholder, phase_train_placeholder, batch_size_placeholder, global_step, 
                     total_loss, train_op, summary_op, summary_writer, regularization_losses, args.learning_rate_schedule_file,
-                    cross_entropy, label_batch, stat, prob_threshold_placeholder, prob_threshold, loss_mean, cross_entropy_orig_mean, learning_rate, normalize_op, weights)
+                    cross_entropy, label_batch, stat, prob_threshold_placeholder, prob_threshold, loss_mean, cross_entropy_orig_mean, learning_rate)
 
                 print('Saving statistics')
                 with h5py.File(stat_file_name, 'w') as f:
@@ -325,7 +333,7 @@ def filter_dataset(dataset, data_filename, percentile, min_nrof_images_per_class
 def train(args, sess, epoch, image_list, label_list, index_dequeue_op, enqueue_op, image_paths_placeholder, labels_placeholder, 
       learning_rate_placeholder, phase_train_placeholder, batch_size_placeholder, global_step, 
       loss, train_op, summary_op, summary_writer, regularization_losses, learning_rate_schedule_file,
-      cross_entropy, label_batch, stat, prob_threshold_placeholder, prob_threshold, cross_entropy_mean, cross_entropy_orig_mean, learning_rate, normalize_op, weights):
+      cross_entropy, label_batch, stat, prob_threshold_placeholder, prob_threshold, cross_entropy_mean, cross_entropy_orig_mean, learning_rate):
     batch_number = 0
     
     if args.learning_rate>0.0:
@@ -352,7 +360,7 @@ def train(args, sess, epoch, image_list, label_list, index_dequeue_op, enqueue_o
             summary_writer.add_summary(summary_str, global_step=step)
         else:
             err, _, step, reg_loss, label_batch_, cross_entropy_mean_, cross_entropy_orig_mean_, lr_, xent_ = sess.run([loss, train_op, global_step, regularization_losses, label_batch, cross_entropy_mean, cross_entropy_orig_mean, learning_rate, cross_entropy], feed_dict=feed_dict)
-        sess.run(normalize_op)
+        #sess.run(normalize_op)
         #w_ = sess.run(weights)
         #print(np.linalg.norm(w_, axis=1))
         start_sample = (step-1) * args.batch_size
@@ -510,6 +518,10 @@ def parse_arguments(argv):
         help='Keep only the classes with this number of examples or more', default=0)
     parser.add_argument('--prob_percentile_threshold', type=float,
         help='Do not use the loss from the prob_percentile_threshold worst probabilities', default=0.0)
+    parser.add_argument('--final_lambda', type=float,
+        help='The weight for normal softmax as opposed to a-softmax', default=1.0)
+    parser.add_argument('--angular_softmax_type', type=int,
+        help='The type of angular softmax to use', default=4)
  
     # Parameters for validation on LFW
     parser.add_argument('--lfw_pairs', type=str,

test/angular_softmax_test.py
@@ -41,7 +41,7 @@ class AngularSoftmaxLossTest(unittest.TestCase):
             weights = tf.placeholder(tf.float32, shape=(nrof_features, nrof_classes), name='weights')
             features = tf.placeholder(tf.float32, shape=(batch_size, nrof_features), name='features')
             labels = tf.placeholder(tf.int32, shape=(batch_size,), name='labels')
-            loss = facenet.angular_softmax_loss_decomp(weights, features, labels, m, lmbd)
+            loss, x_cos_theta, x_psi_theta, x_psi_cos_theta_mix = facenet.angular_softmax_loss_decomp(weights, features, labels, m, lmbd)
             
             sess = tf.Session()
             with sess.as_default():
@@ -59,9 +59,13 @@ class AngularSoftmaxLossTest(unittest.TestCase):
                 #np.testing.assert_array_almost_equal(loss_numpy, loss_ref, decimal=5, err_msg='Numpy vectorized loss does not match reference')
                 
                 # Check tensorflow implementation
-                loss_ = sess.run(loss, feed_dict={weights:W, features:x, labels:yi})
+                loss_, x_cos_theta_, x_psi_theta_, x_psi_cos_theta_mix_ = sess.run([loss, x_cos_theta, x_psi_theta, x_psi_cos_theta_mix], feed_dict={weights:W, features:x, labels:yi})
                 print(loss_ref)
                 print(loss_)
+                
+                print(x_cos_theta_)
+                print(x_psi_theta_)
+                print(x_psi_cos_theta_mix_)
                 
                 np.testing.assert_array_almost_equal(loss_, loss_ref, decimal=5, err_msg='Tensorflow loss does not match reference')
 

tmp/mnist_margin_loss.py
@@ -46,6 +46,7 @@ BATCH_SIZE = 64
 NUM_EPOCHS = 10
 EVAL_BATCH_SIZE = 64
 EVAL_FREQUENCY = 100  # Number of steps between evaluations.
+NROF_DIMS = 128
 
 
 tf.app.flags.DEFINE_boolean("self_test", False, "True if running a self test.")
@@ -178,12 +179,12 @@ def main(argv=None):  # pylint: disable=unused-argument
                             dtype=data_type()))
     fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
     fc1p_weights = tf.Variable(  # fully connected, depth 512.
-        tf.truncated_normal([512, 2],
+        tf.truncated_normal([512, NROF_DIMS],
                             stddev=0.1,
                             seed=SEED,
                             dtype=data_type()))
-    fc1p_biases = tf.Variable(tf.constant(0.1, shape=[2], dtype=data_type()))
-    fc2_weights = tf.Variable(tf.truncated_normal([2, NUM_LABELS],
+    fc1p_biases = tf.Variable(tf.constant(0.1, shape=[NROF_DIMS], dtype=data_type()))
+    fc2_weights = tf.Variable(tf.truncated_normal([NROF_DIMS, NUM_LABELS],
                                                   stddev=0.1,
                                                   seed=SEED,
                                                   dtype=data_type()))
@@ -279,18 +280,26 @@ def main(argv=None):  # pylint: disable=unused-argument
     # controls the learning rate decay.
     batch = tf.Variable(0, dtype=data_type())
 
-    # Training computation: logits + cross-entropy loss.
-    margin_lambda = 1.0 #tf.maximum(1.0 - batch/8500, 0.1) 
     m = 4
+    # NROF_DIMS = 128, final_lambda = 1.00, => Test error = 0.7
+    # NROF_DIMS = 128, final_lambda = 0.98, lambda_decay_start_step = 5000 => Test error = 0.7
+    # NROF_DIMS = 128, final_lambda = 0.96, lambda_decay_start_step = 5000 => Test error = 0.8
+    # NROF_DIMS = 128, final_lambda = 0.94, lambda_decay_start_step = 5000 => Test error = 0.7
+    # NROF_DIMS = 128, final_lambda = 0.8, lambda_decay_start_step = 5000 => Collapsed at 6600
+    # NROF_DIMS = 128, final_lambda = 0.94, lambda_decay_start_step = 0 => Test error = 0.6, 0.7, 0.7
+    # NROF_DIMS = 128, final_lambda = 0.92, lambda_decay_start_step = 0 => Test error = 0.8
+    # NROF_DIMS = 128, final_lambda = 0.90, lambda_decay_start_step = 0 => Test error = 0.8
+    final_lambda = 0.94
+    lambda_decay_start_step = 0
+
+    nrof_steps = int(num_epochs * train_size) // BATCH_SIZE
+    margin_lambda = tf.minimum(1.0, 1.0 - (batch-lambda_decay_start_step)*
+        (1.0-final_lambda)/(nrof_steps-lambda_decay_start_step))
     logits, _ = model(train_data_node, True, m, margin_lambda)
     
-    #logits = batch_norm(logits, True)
+    # Training computation: logits + cross-entropy loss.
     xent_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
         logits=logits, labels=train_labels_node))
-    #beta = 1e-3
-    #center_loss, update_centers = center_loss_op(hidden, train_labels_node)
-    #center_loss, _ = facenet.center_loss(hidden, train_labels_node, 0.95, NUM_LABELS)
-    #loss = xent_loss + beta * center_loss
     loss = xent_loss
   
     # L2 regularization for the fully connected parameters.
@@ -298,6 +307,9 @@ def main(argv=None):  # pylint: disable=unused-argument
                     tf.nn.l2_loss(fc2_weights) + tf.nn.l2_loss(fc2_biases))
     # Add the regularization term to the loss.
     loss += 5e-4 * regularizers
+    
+    #normalize_op = tf.assign(fc2_weights, tf.nn.l2_normalize(fc2_weights, 1))
+
   
     # Decay once per epoch, using an exponential schedule starting at 0.01.
     learning_rate = tf.train.exponential_decay(
@@ -378,6 +390,7 @@ def main(argv=None):  # pylint: disable=unused-argument
                          train_labels_node: batch_labels}
             # Run the graph and fetch some of the nodes.
             #_, l, lr, predictions = sess.run([optimizer, loss, learning_rate, train_prediction], feed_dict=feed_dict)
+            #_, l, lr, predictions, _ = sess.run([optimizer, loss, learning_rate, train_prediction, normalize_op], feed_dict=feed_dict)
             _, l, lr, predictions = sess.run([optimizer, loss, learning_rate, train_prediction], feed_dict=feed_dict)
             if step % EVAL_FREQUENCY == 0:
                 elapsed_time = time.time() - start_time
@@ -397,17 +410,18 @@ def main(argv=None):  # pylint: disable=unused-argument
             print('test_error', test_error)
             assert test_error == 0.0, 'expected 0.0 test_error, got %.2f' % (
                 test_error,)
-            
-        train_embeddings = calculate_embeddings(train_data, sess)
         
-        color_list = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'b', 'g', 'r', 'c' ]
-        h = plt.figure(1)
-        for n in range(0,10):
-            idx = np.where(train_labels[0:10000]==n)
-            plt.plot(train_embeddings[idx,0], train_embeddings[idx,1], color_list[n]+'.')
-        plt.show()
-        h.savefig('/home/david/mnist_margin_loss_m1.png')
-        plt.close()
+        if NROF_DIMS==2:
+            train_embeddings = calculate_embeddings(train_data, sess)
+            
+            color_list = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'b', 'g', 'r', 'c' ]
+            h = plt.figure(1)
+            for n in range(0,10):
+                idx = np.where(train_labels[0:10000]==n)
+                plt.plot(train_embeddings[idx,0], train_embeddings[idx,1], color_list[n]+'.')
+            plt.show()
+            h.savefig('/home/david/mnist_margin_loss_m1.png')
+            plt.close()
 
 
 if __name__ == '__main__':