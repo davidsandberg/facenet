e497475f6e631dc049dbd7d7fa5ed5724a1d290f
iolie <sharon.ruane@intel.com>
2017-11-09 18:39:47 +0000

    Changed output so it works with single face results, also changed naming method

--- test/image_sorting_exports_ALL_faces.py
+++ test/image_sorting_exports_ALL_faces.py
@@ -24,7 +24,7 @@ def initialize_mtcnn(gpu_memory_fraction):
 def align_image(input_image, output_image, pnet, rnet, onet, image_size=182, margin=44, random_order=True,
                 gpu_memory_fraction=1.0, debug=False):
     minsize = 20  # minimum size of face
-    threshold = [0.95, 0.95, 0.99]  # three steps's threshold
+    threshold = [0.7, 0.7, 0.9]  # three steps's threshold
     factor = 0.709  # scale factor
 
     if not os.path.exists(output_image):
@@ -48,44 +48,47 @@ def align_image(input_image, output_image, pnet, rnet, onet, image_size=182, mar
                 det = bounding_boxes[:, 0:4]
                 img_size = np.asarray(img.shape)[0:2]
                 if nrof_faces > 1:
-                    bounding_box_size = (det[:, 2] - det[:, 0]) * (det[:, 3] - det[:, 1])
-                    img_center = img_size / 2
-                    offsets = np.vstack(
-                        [(det[:, 0] + det[:, 2]) / 2 - img_center[1], (det[:, 1] + det[:, 3]) / 2 - img_center[0]])
-                    offset_dist_squared = np.sum(np.power(offsets, 2.0), 0)
-                    # index = np.argmax(
-                    #    bounding_box_size - offset_dist_squared * 2.0)  # some extra weight on the centering
-                    #det = det[index, :]
-                det = np.squeeze(det)
-                counter = 0
-                scaled_list = []
-                for d in det:
+                    det = np.squeeze(det)
+                    counter = 0
+                    scaled_list = []
+                    for d in det:
+                        bb = np.zeros(4, dtype=np.int32)
+                        bb[0] = np.maximum(d[0] - margin / 2, 0)
+                        bb[1] = np.maximum(d[1] - margin / 2, 0)
+                        bb[2] = np.minimum(d[2] + margin / 2, img_size[1])
+                        bb[3] = np.minimum(d[3] + margin / 2, img_size[0])
+                        cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
+                        scaled = scp_misc.imresize(cropped, (image_size, image_size), interp='bilinear')
+                        filename = "{}_{}.jpg".format(output_image.split(".")[0] + "image", str(counter))
+                        scp_misc.imsave(filename, scaled)
+                        scaled_list.append(scaled)
+                        counter = counter +1
+                    return True, scaled_list
+                if nrof_faces == 1:
+                    det = np.squeeze(det)
                     bb = np.zeros(4, dtype=np.int32)
-                    bb[0] = np.maximum(d[0] - margin / 2, 0)
-                    bb[1] = np.maximum(d[1] - margin / 2, 0)
-                    bb[2] = np.minimum(d[2] + margin / 2, img_size[1])
-                    bb[3] = np.minimum(d[3] + margin / 2, img_size[0])
+                    bb[0] = np.maximum(det[0] - margin / 2, 0)
+                    bb[1] = np.maximum(det[1] - margin / 2, 0)
+                    bb[2] = np.minimum(det[2] + margin / 2, img_size[1])
+                    bb[3] = np.minimum(det[3] + margin / 2, img_size[0])
                     cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]
                     scaled = scp_misc.imresize(cropped, (image_size, image_size), interp='bilinear')
-                    scp_misc.imsave(output_image.replace('.jpg', str(counter) + '.jpg'), scaled)
-                    scaled_list.append(scaled)
-                    counter = counter +1
-                return True, scaled_list
+                    scp_misc.imsave(output_image, scaled)
+                    return True, scaled
             else:
                 if debug:
                     print('Unable to align "%s"' % image_path)
 
                 return False, 1
 
+#input_path = '/home/iolie/thorn/sharon/bp_aug_2017_dataset'
+input_path = '/home/iolie/thorn/sharon/oscars'
+output_path = '/home/iolie/thorn/sharon/CHECKS'
+
 input_path = '/home/iolie/thorn/sharon/oscars'
 output_path = '/home/iolie/thorn/sharon'
 
 for filename in os.listdir(input_path):
-   # x = filename.split('_')[0]
-   # directory = (output_path + "/" + x)
-   # if not os.path.exists(directory):
-    #    os.makedirs(directory)
-
     input_image = filename
     output_image = filename
     pnet, rnet, onet = initialize_mtcnn(0.8)
