ed8f332c222354f63f4498551489733f60279b8d
Allen Liu <allen.liu@intel.com>
2017-12-19 10:16:40 +0800

    can get checkpoint info from '${folder}/checkpoint'
    Signed-off-by: Allen Liu <allen.liu@intel.com>

--- src/facenet.py
+++ src/facenet.py
@@ -397,6 +397,11 @@ def get_model_filenames(model_dir):
     elif len(meta_files)>1:
         raise ValueError('There should not be more than one meta file in the model directory (%s)' % model_dir)
     meta_file = meta_files[0]
+    ckpt = tf.train.get_checkpoint_state(model_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+        ckpt_file = os.path.basename(ckpt.model_checkpoint_path)
+        return meta_file, ckpt_file
+
     meta_files = [s for s in files if '.ckpt' in s]
     max_step = -1
     for f in files:
