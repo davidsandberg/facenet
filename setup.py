6bfae8cd790f29ae6b0a3a2ca5d82e5d960471b3
Arman Rahman <arrahm@microsoft.com>
2018-07-24 14:42:14 -0400

    made pypi package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='facenet_sandberg',
+    version='1.0.1',
+    description="Face recognition using TensorFlow",
+    long_description="Face recognition with Google's FaceNet deep neural network & TensorFlow. Mirror of https://github.com/davidsandberg/facenet.",
+    url='https://github.com/armanrahman22/facenet',
+    packages= find_packages(),
+    maintainer='Arman Rahman',
+    maintainer_email='armanrahman22@gmail.com',
+    include_package_data=True,
+    license='MIT',
+    install_requires=[
+        'tensorflow', 'scipy', 'scikit-learn', 'opencv-python',
+        'h5py', 'matplotlib', 'Pillow', 'requests', 'psutil'
+    ]
+)
