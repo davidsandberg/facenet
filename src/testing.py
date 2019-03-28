503743470a9167cc40129c8183e777106725ce78
ddel <ddel@itu.dk>
2019-03-28 21:30:05 +0100

    lmao

--- src/testing.py
+++
@@ -1,8 +0,0 @@
-from src.facedetector import face_detector
-
-images = ['/home/dandy/PycharmProjects/facenet/data/images/t1.jpg', '/home/dandy/PycharmProjects/facenet/data/images/t2.jpg', '/home/dandy/PycharmProjects/facenet/data/images/t3.jpg']
-aligned = face_detector.align_face(images)
-comparisons = face_detector.compare(aligned, 0.3)
-
-print("Is image 1 and 2 similar? ", bool(comparisons[0][1]))
-print("Is image 1 and 3 similar? ", bool(comparisons[0][2]))
