2a35934a699061d0bf83b787c7c05cc3a11108ff
Gernot Howanitz <gern@aon.at>
2021-02-02 23:20:31 +0100

    debug setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='facenet',
     version='0.1.0',
-    packages=find_packages(include=['facenet', 'facenet.*']),
+    packages=find_packages(),
     install_requires=[
         "absl-py==0.11.0",
         "astunparse==1.6.3",
